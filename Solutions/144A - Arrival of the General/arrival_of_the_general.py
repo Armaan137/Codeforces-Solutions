72c3fecbf02423e817fb301e5a2dd2879ff561e4
Armaan137 <a.khabra5@gmail.com>
2023-06-26 14:53:10 -0600

    Add problem 144A

---
+++ Solutions/144A - Arrival of the General/arrival_of_the_general.py
@@ -0,0 +1,27 @@
+# This problem can be found here: https://codeforces.com/problemset/problem/144/A.
+
+def main():
+    n = int(input())
+    heights = list(map(int, input().split()))
+    h = heights[0]
+    minh = maxh = h
+    mini = maxi = 0
+
+    for i in range(1, n):
+        h = heights[i]
+        if h > maxh:
+            maxh = h
+            maxi = i
+        if h <= minh:
+            minh = h
+            mini = i
+    if mini < maxi:
+        result = maxi + (n - 1 - mini) - 1
+    else:
+        result = maxi + (n - 1 - mini)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
